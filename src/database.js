da52f8186cefce4693b3292901f393a69336f06e
Daniel Esau Rivera Ayala <danielriveraayala@gmail.com>
2020-06-21 00:33:13 -0500

    update server & database js

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env;
 
-const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
+const MONGODB_URI = `mongodb://localhost/note_app`;
 //zdPino8UwE4FPqPZ
 //notas_app
 mongoose
