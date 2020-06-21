d349a1c44c0b0f0edb71fef3e0e37a570fa392b0
Daniel Esau Rivera Ayala <danielriveraayala@gmail.com>
2020-06-21 02:29:47 -0500

    heroku

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const app = require('./server');
 require('./database');
 
 // Server is listening
+app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), () => {
   console.log('Server on port', app.get('port'));
   //console.log('Environment:', process.env.NODE_ENV);
